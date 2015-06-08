7d4c55a7fb756f2d937a047e461c6e06fb0246fc
Joseph Nudell <joenudell@gmail.com>
2015-06-08 11:23:30 -0500

    Remove newlines, except at end of file

--- index.js
+++ index.js
@@ -29,20 +29,20 @@ module.exports = function (src, opts, fn) {
     src = src === undefined ? opts.source : src;
     if (typeof src !== 'string') src = String(src);
     var ast = parse(src, opts);
-    
+
     var result = {
         chunks : src.split(''),
         toString : function () { return result.chunks.join('') },
         inspect : function () { return result.toString() }
     };
     var index = 0;
-    
+
     (function walk (node, parent) {
         insertHelpers(node, parent, result.chunks);
-        
+
         forEach(objectKeys(node), function (key) {
             if (key === 'parent') return;
-            
+
             var child = node[key];
             if (isArray(child)) {
                 forEach(child, function (c) {
@@ -57,17 +57,17 @@ module.exports = function (src, opts, fn) {
         });
         fn(node);
     })(ast, undefined);
-    
+
     return result;
 };
- 
+
 function insertHelpers (node, parent, chunks) {
     node.parent = parent;
-    
+
     node.source = function () {
         return chunks.slice(node.start, node.end).join('');
     };
-    
+
     if (node.update && typeof node.update === 'object') {
         var prev = node.update;
         forEach(objectKeys(prev), function (key) {
@@ -78,10 +78,14 @@ function insertHelpers (node, parent, chunks) {
     else {
         node.update = update;
     }
-    
+
     function update (s) {
         chunks[node.start] = s;
         for (var i = node.start + 1; i < node.end; i++) {
+            chunks[i] = '';
+        }
+
+        if (chunks[i] === '\n' && chunks[i + 1]) {
             chunks[i] = '';
         }
     }
